570c438ae067e804bb8fe051ff5036ce7fd238f8
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2025-02-15 16:47:21 -0300

    Added Enemy Colliders and startup animation
    Agora o inimigo faz uma escala de crescimento, quando o jogo começa

--- Assets/Script/Enemy/EnemyBase.cs
+++ Assets/Script/Enemy/EnemyBase.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using DG.Tweening;
 
 
 namespace Enemy
@@ -11,6 +12,11 @@ namespace Enemy
         public float startLife = 10f;
 
         [SerializeField] private float _currentLife;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
 
         private void Awake()
         {
@@ -26,6 +32,9 @@ namespace Enemy
         protected virtual void Init()
         {
             ResetLife();
+
+            if(startWithBornAnimation)
+                BornAnimation();
         }
 
         protected virtual void Kill()
@@ -47,6 +56,19 @@ namespace Enemy
                 Kill();
             }
         }
+
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+
+
+
+        #endregion
+
+
 
         private void Update()
         {
