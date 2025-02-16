7bc6021459bc3baf1a171a1a3dd3653b1a3219b3
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2025-02-16 17:04:54 -0300

    Added second enemy Type
    - Adicionado Enemy 02, que é invisível
    - Quando invisível, ele não tem colisor para sofrer dano
    - Quando o jogador se aproxima, o inimigo fica visível e é passível de sofrer dano

---
+++ Assets/Script/Enemy/EnemyInvisible.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemy;
+
+public class EnemyInvisible : EnemyBase
+{
+    public MeshRenderer meshRenderer;
+    public BoxCollider boxCollider;
+
+    private void Start()
+    {
+        SetAlpha(0f);
+        boxCollider.enabled = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SetAlpha(1f); // Torna visível
+            boxCollider.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SetAlpha(0f); // Torna invisível
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        Color newColor = meshRenderer.material.color; // Obtém a cor atual
+        newColor.a = alpha; // Modifica apenas o Alpha
+        meshRenderer.material.color = newColor; // Aplica a nova cor
+
+        // Se o material usa transparência via shader
+        meshRenderer.material.SetFloat("_Mode", 3); // Define como transparente no Standard Shader
+    }
+
+}
